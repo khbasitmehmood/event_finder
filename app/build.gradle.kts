df5996817aeb4c27f707ccc1c464cfa888f1a9a5
Basit <kh.basitmehmood@gmail.com>
2026-06-30 22:11:12 +0500

    Ai fixes for chatbot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.storage)
+    implementation(libs.firebase.ai)
     implementation(libs.firebase.messaging)
 
     // Maps & Location
